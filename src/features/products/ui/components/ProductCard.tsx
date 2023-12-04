d097978e3c42da41e2f256a96dd4b9ab6c392864
Eldiiar Temir <etemir@nurtelecom.kg>
2023-12-04 18:49:16 +0600

    add styles

--- src/features/products/ui/components/ProductCard.tsx
+++ src/features/products/ui/components/ProductCard.tsx
@@ -2,19 +2,36 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import { ProductModel } from "../../domain/product.model";
 import "./styles/ProductCard.styles.css";
+import { Col, Row } from "react-bootstrap";
 interface ProductCardProps {
   product: ProductModel;
 }
 
 export const ProductCard: React.FC<ProductCardProps> = ({
-  product: { image, title },
+  product: {
+    image,
+    title,
+    price,
+    rating: { count, rate },
+  },
 }) => {
   return (
-    <Card>
-      <Card.Img variant="top" src={image} className="product_card-image" />
+    <Card className="product-card">
+      <Card.Img variant="top" src={image} className="image" />
       <Card.Body>
-        <Card.Title className="product_card-title">{title}</Card.Title>
+        <Card.Title className="title">{title}</Card.Title>
+        <Card.Text className="price">${price}</Card.Text>
       </Card.Body>
+      <Card.Footer>
+        <Row className="justify-content-start">
+          <Col xs="auto">
+            <Card.Text className="rating-text">{count} sold</Card.Text>
+          </Col>
+          <Col xs="auto">
+            <Card.Text className="rating-text">{rate} rating</Card.Text>
+          </Col>
+        </Row>
+      </Card.Footer>
     </Card>
   );
 };
