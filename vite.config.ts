aa1256dec5054f8b6782882eea2c44cdd350f0df
Eldiiar Temir <etemir@nurtelecom.kg>
2023-12-06 13:08:03 +0600

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import * as path from "path";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
-// import { VitePWA } from "vite-plugin-pwa";
+import { VitePWA } from "vite-plugin-pwa";
 
 const proxy = {
   "/api": {
@@ -13,7 +13,6 @@ const proxy = {
 };
 
 export default defineConfig({
-  base: "./",
   preview: {
     port: 1234,
   },
@@ -21,9 +20,6 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       output: {
-        chunkFileNames: "js/[name].[hash].js",
-        entryFileNames: "js/[name].[hash].js",
-        assetFileNames: "assets/[name].[hash].[ext]",
         manualChunks: {
           reactVendor: ["react", "react-dom", "react-router-dom"],
         },
@@ -34,25 +30,25 @@ export default defineConfig({
     proxy,
   },
   plugins: [
-    // VitePWA({
-    //   workbox: {
-    //     globPatterns: ["**/*.{js,css,html,svg}"],
-    //     runtimeCaching: [
-    //       {
-    //         urlPattern: ({ url }) => {
-    //           return url.pathname.startsWith("/api");
-    //         },
-    //         handler: "CacheFirst" as const,
-    //         options: {
-    //           cacheName: "api-cache",
-    //           cacheableResponse: {
-    //             statuses: [0, 200],
-    //           },
-    //         },
-    //       },
-    //     ],
-    //   },
-    // }),
+    VitePWA({
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg}"],
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => {
+              return url.pathname.startsWith("/api");
+            },
+            handler: "CacheFirst" as const,
+            options: {
+              cacheName: "api-cache",
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
+      },
+    }),
     svgr({
       include: "**/*.svg",
     }),
